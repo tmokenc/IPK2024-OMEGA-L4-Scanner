f0ac757f25a2acbbdce79fe5d19583ac07cafc0b
Nguyen Le Duy <xnguye27@stud.fit.vut.cz>
2024-04-17 19:35:22 +0200

    feat: Add support for scanning UDP port with IPv4

--- udp.c
+++ udp.c
@@ -4,6 +4,7 @@
 #include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
 #include <netinet/udp.h>
+#include <arpa/inet.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <sys/socket.h>
@@ -11,6 +12,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <poll.h>
+#include "time.h"
 
 /// 8 KiB
 #define PACKET_LEN (1 << 13)
@@ -23,6 +25,15 @@ struct udp_header {
     uint16_t checksum;
 };
 
+// Structure representing the UDP pseudo-header
+struct pseudo_udp_header {
+    uint32_t src_addr;
+    uint32_t dst_addr;
+    uint8_t zeroes;
+    uint8_t protocol;
+    uint16_t udp_length;
+};
+
 int make_header(char *packet, struct sockaddr *src, socklen_t src_len, struct sockaddr *dst, socklen_t dst_len, uint16_t port) {
     // Construct IP header
     struct ip *ip_header = (struct ip *)packet;
@@ -30,23 +41,28 @@ int make_header(char *packet, struct sockaddr *src, socklen_t src_len, struct so
     ip_header->ip_v = 4; // IPv4
     ip_header->ip_tos = 0;
     ip_header->ip_len = htons(sizeof(struct ip) + sizeof(struct udphdr));
-    ip_header->ip_id = htons(0);
+    ip_header->ip_id = htons(54321);
     ip_header->ip_off = 0;
     ip_header->ip_ttl = 64;
     ip_header->ip_p = IPPROTO_UDP;
     ip_header->ip_sum = 0; // TBD later
 
     // Set source and destination addresses
-    memcpy(&ip_header->ip_src, src, src_len);
-    memcpy(&ip_header->ip_dst, dst, dst_len);
+    ip_header->ip_src.s_addr = inet_addr(inet_ntoa(((struct sockaddr_in *)src)->sin_addr));
+    ip_header->ip_dst.s_addr = inet_addr(inet_ntoa(((struct sockaddr_in *)dst)->sin_addr));
+
+    // (void)(src_len);
+    // (void)(dst_len);
+    // memcpy(&ip_header->ip_src, src, src_len);
+    // memcpy(&ip_header->ip_dst, dst, dst_len);
 
     // Construct UDP header
     struct udp_header *udp_header = (struct udp_header *)(packet + sizeof(struct ip));
     udp_header->src_port = htons(SOURCE_PORT);
     udp_header->dst_port = htons(port);
     udp_header->len = htons(sizeof(struct udp_header));
-    udp_header->checksum = 0; // Kernel will compute the checksum
-    
+    udp_header->checksum = 0; // Ignore for IPv4
+
     ip_header->ip_sum = checksum((uint16_t *)packet, sizeof(struct ip) + sizeof(struct udphdr));
 
     return sizeof(struct ip) + sizeof(struct udphdr);
@@ -106,8 +122,16 @@ void udp_scan(
     fd.fd = recvfd;
     fd.events = POLLIN;
 
+    Timestamp start = timestamp_now();
+
     while (1) {
-        int poll_result = poll(&fd, 1, timeout);
+        int wait_time = timeout - timestamp_elapsed(start);
+
+        if (wait_time < 0) {
+            wait_time = 0;
+        }
+
+        int poll_result = poll(&fd, 1, wait_time);
 
         if (poll_result < 0) {
             perror("select");
@@ -118,7 +142,7 @@ void udp_scan(
             break;
         } else {
             struct sockaddr recv_addr;
-            socklen_t recv_addr_len;
+            socklen_t recv_addr_len = sizeof(recv_addr);
             // Data is available to read from recvfd
             ssize_t bytes_received = recvfrom(recvfd, recv_packet, PACKET_LEN, 0, &recv_addr, &recv_addr_len);
             if (bytes_received < 0) {
@@ -131,12 +155,18 @@ void udp_scan(
             size_t ip_header_length = ip_header->ip_hl * 4;
             struct icmp *icmp_packet = (struct icmp *)(recv_packet + ip_header_length);
 
-            // TODO check package
-            
             if (icmp_packet->icmp_type != ICMP_UNREACH) {
                 // Received ICMP packet, but not of type 3\n
                 continue;
             }
+
+            // Check if ICMP packet came from the expected address
+            if (memcmp(&((struct sockaddr_in *)&recv_addr)->sin_addr, &((struct sockaddr_in *)dst_addr)->sin_addr, sizeof(struct in_addr)) != 0) {
+                printf("Received ICMP packet from unexpected address\n");
+                print_address(&recv_addr);
+                printf("\n");
+                continue;
+            } 
 
             printf("%d/udp closed\n", port);
             break;
