ef9b9495254206a7d8256a34c6f0ecbd43a22e52
Nguyen Le Duy <xnguye27@stud.fit.vut.cz>
2024-04-17 19:55:01 +0200

    refactor: don't use raw header

--- udp.c
+++ udp.c
@@ -35,37 +35,18 @@ struct pseudo_udp_header {
 };
 
 int make_header(char *packet, struct sockaddr *src, socklen_t src_len, struct sockaddr *dst, socklen_t dst_len, uint16_t port) {
-    // Construct IP header
-    struct ip *ip_header = (struct ip *)packet;
-    ip_header->ip_hl = 5;
-    ip_header->ip_v = 4; // IPv4
-    ip_header->ip_tos = 0;
-    ip_header->ip_len = htons(sizeof(struct ip) + sizeof(struct udphdr));
-    ip_header->ip_id = htons(54321);
-    ip_header->ip_off = 0;
-    ip_header->ip_ttl = 64;
-    ip_header->ip_p = IPPROTO_UDP;
-    ip_header->ip_sum = 0; // TBD later
-
-    // Set source and destination addresses
-    ip_header->ip_src.s_addr = inet_addr(inet_ntoa(((struct sockaddr_in *)src)->sin_addr));
-    ip_header->ip_dst.s_addr = inet_addr(inet_ntoa(((struct sockaddr_in *)dst)->sin_addr));
-
-    // (void)(src_len);
-    // (void)(dst_len);
-    // memcpy(&ip_header->ip_src, src, src_len);
-    // memcpy(&ip_header->ip_dst, dst, dst_len);
-
-    // Construct UDP header
-    struct udp_header *udp_header = (struct udp_header *)(packet + sizeof(struct ip));
+    struct udp_header *udp_header = (struct udp_header *)(packet);
     udp_header->src_port = htons(SOURCE_PORT);
     udp_header->dst_port = htons(port);
     udp_header->len = htons(sizeof(struct udp_header));
     udp_header->checksum = 0; // Ignore for IPv4
+                              
+    (void)dst;
+    (void)src;
+    (void)dst_len;
+    (void)src_len;
 
-    ip_header->ip_sum = checksum((uint16_t *)packet, sizeof(struct ip) + sizeof(struct udphdr));
-
-    return sizeof(struct ip) + sizeof(struct udphdr);
+    return sizeof(struct udphdr);
 }
 
 void udp_scan(
@@ -82,12 +63,6 @@ void udp_scan(
     }
 
     const int enable = 1;
-    if (setsockopt(sockfd, IPPROTO_IP, IP_HDRINCL, &enable, sizeof(int))) {
-        perror("setsockopt");
-        close(sockfd);
-        return;
-    }
-
     if (setsockopt(recvfd, IPPROTO_IP, IP_HDRINCL, &enable, sizeof(int))) {
         perror("setsockopt");
         close(sockfd);
